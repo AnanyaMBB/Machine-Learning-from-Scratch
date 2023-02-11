d23ac27004ba4d45a8862449c118910df2753c17
Ananya <ananyabesufekad@gmail.com>
2023-02-11 21:39:26 +0900

    Method setup
    setting up the parameter dimensions and forward propagation method

--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+class NeuralNetwork:
+    def __init__(self, layers, units, data):
+        self.w = []
+        self.b = []
+        self.layers = layers
+        self.units = units
+        self.data = data
+
+    # Activation functions
+    def sigmoid(z):
+        return np.exp(-z)
+
+    # layers
+    def Dense(self):
+        for i in range(self.layers - 1):
+            self.w.append(np.zeros((self.units[i], self.units[i+1])))
+            self.b.append(np.zeros())
+        self.w = np.array(self.w)
+        self.b = np.zeros((self.layers, ))
+
+    # Forward propagation
+    def start_forward_prop(self):
+        for i in range(self.layers):
+            self.forward_prop(self, self.data, self.w, self.b)
+
+    def forward_prop(self, a, w, b):
+        np.matmul(self.data.T, self.w) + self.b
+
+    # Backward propagation
+
+    # def test():
+    #     print(self.w)
+
+
+a = NeuralNetwork(3, [2, 3, 1])
+print(a.w)
